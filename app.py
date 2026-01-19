68578ed9489de937ab211cfacc4f32bdff073c5b
Dhiman Tarafdar <dhimantarafdar23@gmail.com>
2026-01-19 21:56:55 +0600

    fixed interface in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# Gradio app 
+#gradio app 
+
 import gradio as gr
 import pandas as pd
 import pickle
@@ -9,36 +10,40 @@ with open("insurance_gb_pipeline.pkl", "rb") as f:
     model = pickle.load(f)
 
 # 2. The Logic Function
-def predict_insurance_cost(age, sex, bmi, children, smoker, region):
+def predict_gpa(age, sex, bmi, children, smoker, region):
     
     # Pack inputs into a DataFrame
+    # The column names must match your CSV file exactly
     input_df = pd.DataFrame([[
         age, sex, bmi, children, smoker, region
+
     ]],
       columns=[
-        'age', 'sex', 'bmi', 'children', 'smoker', 'region'
+        'age','sex','bmi','children','smoker','region'
     ])
     
     # Predict
     prediction = model.predict(input_df)[0]
     
-    # Return formatted result
-    return f"Predicted Insurance Cost: ${prediction:.2f}"
+    # Return formatted result (Clipped 0-5)
+    return f"Predicted Insurance Cost: {np.clip(prediction, 0, None):.2f}"
+    
 
 # 3. The App Interface
+# Defining inputs in a list to keep it clean
 inputs = [
-    gr.Number(label="Age", value=30),
-    gr.Radio(["male", "female"], label="Sex"),
-    gr.Number(label="BMI", value=25.0),
-    gr.Slider(0, 5, step=1, label="Children"),
-    gr.Radio(["yes", "no"], label="Smoker"),
-    gr.Dropdown(["southwest", "southeast", "northwest", "northeast"], label="Region")
+        gr.Slider(18, 64, step=1, label="Age"),
+        gr.Dropdown(["male","female"], label="Sex"),
+        gr.Slider(16, 53.1, step=0.1, label="BMI"),
+        gr.Slider(0, 5, step=1, label="Children"),
+        gr.Dropdown(["yes","no"], label="Smoker"),
+        gr.Dropdown(["southwest","southeast","northwest","northeast"], label="Region")
 ]
 
 app = gr.Interface(
-    fn=predict_insurance_cost,
+    fn=predict_gpa,
       inputs=inputs,
         outputs="text", 
-        title="Medical Insurance Cost Predictor")
+        title="Insurance Cost Predictor")
 
 app.launch(share=True)
